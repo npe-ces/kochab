fb6b04d9a9fbf465b7af7b5bf70f5d187253873a
npe-ces <43264952+npe-ces@users.noreply.github.com>
2018-11-12 23:26:17 +0900

    백엔드단 수정(디비 연결은 조금 더 손봐야함)

--- be/src/main/java/io/kochab/Application.java
+++ be/src/main/java/io/kochab/Application.java
@@ -1,4 +1,4 @@
-package io.kochab.webservice;
+package io.kochab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- be/src/main/java/io/kochab/webservice/Controller.java
+++ be/src/main/java/io/kochab/webservice/Controller.java
@@ -1,6 +1,5 @@
 package io.kochab.webservice;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
